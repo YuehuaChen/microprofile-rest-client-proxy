02894cd3c3ca5ca333a5c25dd54b51b34e7b713b
gituser <catchingmouse@gmail.com>
2023-01-10 16:39:20 +0100

    https proxy with basic authentication

--- src/main/java/com/demo/rest/HttpBinResource.java
+++ src/main/java/com/demo/rest/HttpBinResource.java
@@ -5,6 +5,8 @@ import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 import java.net.URI;
 
 @ApplicationScoped
@@ -12,10 +14,19 @@ import java.net.URI;
 public class HttpBinResource {
     @GET
     public MyRequest getRequest(){
+        System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                if(RequestorType.PROXY.equals(getRequestorType())) {
+                    return new PasswordAuthentication("test_user", "T0pS3cr3t".toCharArray());
+                }
+                return null;
+            }
+        });
         HttpBinService service = RestClientBuilder.newBuilder()
-                .baseUri(URI.create("http://httpbin.org"))
-                .proxyAddress("ec2-15-237-127-47.eu-west-3.compute.amazonaws.com", 3128)
-                .register(ProxyAuthorizationRequestFilter.class)
+                .baseUri(URI.create("https://httpbin.org"))
+                .proxyAddress("ec2-35-180-92-16.eu-west-3.compute.amazonaws.com", 3128)
                 .build(HttpBinService.class);
         return service.getRequest("1");
     }
